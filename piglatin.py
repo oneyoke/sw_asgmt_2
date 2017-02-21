fb3774f1d2c42cda1bfddc52800f7e4d2b995def
Pavel <pavel.anistratov@liu.se>
2017-02-21 14:25:33 +0100

    parserCommandLine function

--- piglatin.py
+++ piglatin.py
@@ -1,6 +1,10 @@
 import sys
 
-latin = sys.argv[1] if len(sys.argv) > 1 else ""
+def parseCommandLine(argv):
+    return argv[1] if len(argv) > 1 else ""
 
-print(latin)
-print("igpay atinlay")
+
+if __name__ == "__main__":
+    latin = parseCommandLine(sys.argv)
+    print(latin)
+    print("igpay atinlay")
